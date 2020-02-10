453b38d091191bc1623a06fc0ad5ec836913ce13
Alan Michel Solorzano Tapia <K697344@nac.ppg.com>
2020-02-10 16:12:55 -0600

    Agregar Solucion

--- ConsolaEntityFramework/Program.cs
+++ ConsolaEntityFramework/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,23 @@ namespace ConsolaEntityFramework
     {
         static void Main(string[] args)
         {
+            int id = 0;
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Monto");
+            dt.Columns.Add("Concepto");
+            dt.Columns.Add("Direccion");
+            List<SPGetFactura_Result> listFactura = new List<SPGetFactura_Result>();
+
+            using (PruebaEntities entity = new PruebaEntities())
+            {
+                listFactura = entity.SPGetFactura(id).ToList();
+            }
+
+            foreach(var fac in listFactura)
+            {
+                dt.Rows.Add(fac.Id, fac.Monto, fac.Concepto, fac.Direccion);
+            }
         }
     }
 }
